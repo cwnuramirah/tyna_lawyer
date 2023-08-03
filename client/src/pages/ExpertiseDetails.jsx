af30a2bcf2ea85a9dabe5e8290fdf97e8d262598
cwnuramirah <cw.nuramirah@gmail.com>
2023-08-03 20:46:46 +0800

    expert details page styling

--- client/src/pages/ExpertiseDetails.jsx
+++ client/src/pages/ExpertiseDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { ArrowLeft } from 'react-feather'
 import { Link, useParams } from 'react-router-dom'
+import BlockContent from '@sanity/block-content-to-react';
 import client from '../client';
 
 const ExpertiseDetails = () => {
@@ -13,7 +14,8 @@ const ExpertiseDetails = () => {
 			`*[_type == 'expertise' && slug.current == '${expertiseSlug}']{
 				practice,
 				brief,
-				'headOfDep': head->fullName
+				'headOfDep': head->fullName,
+				content
 			}
 			[0]`
 		)
@@ -26,19 +28,24 @@ const ExpertiseDetails = () => {
 	useEffect(() => {
 		getExpertiseData();
 	}, [])
-	
+
 	return (
-		<main>
-			<section>
-				<Link to={-1}>
+		<main className='expertiseDetails'>
+			<section className='expertiseDetails--header'>
+				<Link to={-1} className='expertiseDetails--header_backLink'>
 					<ArrowLeft />
 					<p><strong>Back to Expertise</strong></p>
 				</Link>
 				<h1>{details["practice"]}</h1>
-				<article>
-					{details["brief"]}
-				</article>
-
+			</section>
+			<section className='expertiseDetails--body'>
+				<div className='expertiseDetails--body_article'>
+						<BlockContent blocks={details["content"]} />
+				</div>
+				<div className='expertiseDetails--body_info'>
+						<div className='expertiseDetails--body_info_image'></div>
+						<p className='expertiseDetails--body_info_name'>{details['headOfDep']}</p>
+				</div>
 			</section>
 		</main>
 	)
