a75b052fe3884ee3aad640d7e3662813811ba639
cwnuramirah <cw.nuramirah@gmail.com>
2023-08-05 17:45:00 +0800

    fix splash

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,47 @@ import Navbar from './components/Navbar';
 import Expertise from './pages/Expertise';
 import Home from './pages/Home';
 import ExpertiseDetails from './pages/ExpertiseDetails';
+import Splash from './components/Splash';
+import { useEffect, useState } from 'react';
 
 const Layout = () => {
+	const SPLASH_ANIMATION_DURATION = 16 * 1000
+	const [visible, setVisible] = useState(false)
+
+
+	useEffect(() => {
+		// Check if the splash screen ever initiated before this
+
+		// If the splash screen had initiated before, local storage should save the splash state as true.
+		// Therefore, no more splash screen initiated through this session.
+		if (window.localStorage.getItem('splash') !== null) {
+			setVisible(JSON.parse(window.localStorage.getItem('splash')));
+		} else {
+			setVisible(false)
+			window.localStorage.setItem('splash', false);
+		}
+
+		// initiate splash screen
+		if (visible === false) {
+			setTimeout(() => {
+				setVisible(true);
+				window.localStorage.setItem('splash', true);
+			}, SPLASH_ANIMATION_DURATION)
+		}
+
+	}, []);
+
+
 	return (
 		<div className='app'>
-			<Navbar />
-			<Outlet />
-			<Footer />
+			<div className={visible ? 'hide-content' : 'display-content'}>
+				<Splash />
+			</div>
+			<div className={visible ? 'display-content' : 'hide-content'}>
+				<Navbar />
+				<Outlet />
+				<Footer />
+			</div>
 		</div>
 	)
 }
---
+++ client/src/components/Splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Splash = () => {
+  return (
+	<div className='splash'>
+		<h2 className='splash--text'>Client-Centric Solutions</h2>
+		<h2 className='splash--text'>Uncompromising Integrity.</h2>
+		<h2 className='splash--text'>Technology-Driven Approach.</h2>
+		<h2 className='splash--text'>Taufiq Yong, Advocates and Solitors</h2>
+	</div>
+  )
+}
+
+export default Splash
