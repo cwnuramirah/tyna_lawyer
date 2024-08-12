382eb543b8e9a813a9f113f2605ea02137e9f4fa
cwnuramirah <cw.nuramirah@gmail.com>
2024-08-12 14:13:25 +0800

    fetch api for About page

--- api/schemas/layout.js
+++ api/schemas/layout.js
@@ -1,18 +1,30 @@
 export const LAYOUT = [
 	{ title: 'Content on Center', 
-		value: 'TC'},
-	{ title: 'Title with Content on Right', value: 'T-C'},
-	{ title: 'Content with Image on Right', value: 'T I - C I'},
-	{ title: 'Content with Image on Left', 	value: 'I T - I C'},
-	{ title: 'Diagonal Right to Left', 		value: 'T X - X C'},
-	{ title: 'Diagonal Right to Left with Image on center', value: 'T X - I I - X C'},
+		value: 'T C'},
+	{ title: 'Title with Content on Right', value: 'T C-T C'},
+	{ title: 'Content with Image on Right', value: 'T I-C I'},
+	{ title: 'Content with Image on Left', 	value: 'I T-I C'},
+	{ title: 'Diagonal Right to Left', 		value: 'T X-X C'},
+	{ title: 'Diagonal Right to Left with Image on center', value: 'T X-I I-X C'},
 ]
+
+/*
+	VALUE FORMAT:
+		ALWAYS USE 2 COLUMN FORMAT, 
+		WHITESPACE IS IMPORTANT
+			X X
+
+		FOR NEW ROW, ADD `-`
+			X X-X X 	(2 ROW, 2X2 GRID)
+			X X-X X-X X (3 ROW, 2X3 GRID)
+ */
 
 export function hideImageField(layout) {
 	if(layout == undefined) {
 		return true
 	}
 
+	// Test if there's an I (image) in layout
 	const regex = /([I])/gm
 	if(regex.test(layout)){
 		return false
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,98 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { Check } from 'react-feather';
-// import client from '../client';
-// import BlockContent from '@sanity/block-content-to-react';
+import React from 'react';
+import { useAboutContent } from '../data/useAboutContent';
+import { useImageUrlBuilder } from '../data/useImageUrlBuilder';
 
 const About = () => {
-	// const [content, setContent] = useState([]);
+	const { content } = useAboutContent();
+	const { urlFor } = useImageUrlBuilder();
 
-	// async function getContent() {
-	// 	const res = await client.fetch(
-	// 		`
-	// 			*[_type == 'about']{
-	// 				introduction
-	// 			}[0]
-	// 		`
-	// 	)
-	// 		.then((data) => setContent(data))
-	// 		.catch((err) => console.log(err));
+	function getGridTemplate(layout) {
+		let gridTemplate = layout.replace(/-/g, `""`)
+		gridTemplate = gridTemplate.replace(/X/g, ".")
+		gridTemplate = `"`.concat(gridTemplate);
+		gridTemplate = gridTemplate.concat(`"`);
+		return gridTemplate;
+	}
 
-	// 	return res;
-	// };
+	const about = content.map((section, index) => {
+		const classname = section.title.replace(/\s/g, "-");
+		const gridTemplate = getGridTemplate(section.layout);
+		const sectionStyle = {
+			display: 'grid',
+			gridTemplateAreas: gridTemplate,
+			gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+			gap: '2vw'
+		}
 
-	// useEffect(() => {
-	// 	getContent();
-	// }, [])
+		return <section key={index} className={classname} style={sectionStyle}>
+			<h2 style={{alignSelf: section.image !== null ? 'end' : 'start'}}>{section.heading}</h2>
+			<p style={{alignSelf: 'start'}}>{section.text}</p>
+			{
+				section.image !== null &&
+				<div className='section_image'>
+					<img src={urlFor(section.image)} alt={'Taufiq Yong & Associates | ' + section.title} />
+				</div>
+			}
+		</section>
+	})
 
 	return (
 		<main className='about'>
-			<header>
-				<h1>About Us</h1>
-			</header>
-			<section>
-				<h4 className='text--long'>
-					Taufiq Yong & Associates is a law firm practising employment law, corporate and
-					commercial advisory and dispute resolution. Since our inception in 2014, we have
-					grown exponentially and have served thousands of satisfied clients worldwide.
-				</h4>
-			</section>
-			<section className='about--principles'>
-				<div className='about--principles_header'>
-					<h2>Our Central Principles</h2>
-				</div>
-
-				<ul>
-					<li>
-						<h3><Check /></h3>
-						<div>
-							<h3 className=''>Client-Centric Solutions</h3>
-							<p className='text--long'>
-								Beyond expertise, we're committed to understanding your unique needs and crafting
-								tailored solutions. With clear communication and proactive strategies, we ensure your
-								peace of mind every step of the way.
-							</p>
-						</div>
-					</li>
-					<li>
-						<h3><Check /></h3>
-						<div>
-							<h3 className=''>Uncompromising Integrity.</h3>
-							<p className='text--long'>
-								At Taufiq Yong & Associates, integrity is not just a goal—it’s our foundation.
-								We uphold the highest ethical standards, ensuring transparent, trustworthy, and
-								accountable legal services for every client. Choose a firm that prioritizes your
-								trust and delivers results with unwavering integrity.
-							</p>
-						</div>
-					</li>
-					<li>
-						<h3><Check /></h3>
-						<div>
-							<h3 className=''>Technology-Driven Approach.</h3>
-							<p className='text--long'>
-								To stay agile in an increasingly hectic world, we manage all our
-								documents and projects using leading-edge technology. Our commitment
-								to bringing value to our clients is evident in how we constantly
-								innovate to achieve greater productivity. We pass on these benefits
-								to our clients who are assured to receive prompt and efficient services, always.</p>
-						</div>
-
-					</li>
-				</ul>
-			</section>
-			<section>
-				<h2>Dedicated to Serve Our Community</h2>
-				<p>
-					We are passionate about educating the public and making legal knowledge accessible to
-					everyone. From publishing over hundreds of free legal articles on our website to producing
-					a 2-minute video series, #LegalFlix, our initiatives have reached over a million viewers
-					globally. We have also partnered with Lexis Nexis to set up a COVID-19 resource hub for
-					employment law issues during the pandemic.
-				</p>
-			</section>
-			{/* <section>
-				<BlockContent blocks={content["introduction"]} />
-			</section> */}
+			{
+				content.length !== 0 ?
+					about
+					: <section>
+						<p>Loading our story...</p>
+					</section>
+			}
 		</main>
 	)
 }
